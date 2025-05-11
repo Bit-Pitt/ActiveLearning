2445dab5bc9e6115d6ba83d7569fa8e3675700ca
Pietro <pietrofoggia03@gmail.com>
2025-05-11 10:08:07 +0200

    .

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ActiveLearning
 Implementation of a active learning uncertainty sampling
 This is an a simple example, it will use two different type un uncertainty sampling for multi-label problem.
+Necessary Packages :
+%pip install contractions --quiet          , with reboot if needed
--- binary_US/evaluation.py
+++ binary_US/evaluation.py
@@ -25,13 +25,15 @@ def evaluation(model,X_test,y_test,ifplot):
     moc_results = evaluate_label_wise(y_test, y_pred, metrics)
     print(moc_results)
 
+    '''
     moc_results_2 = evaluate(y_test, y_pred, {
         "hamming_loss" : hamming_loss,
         "accuracy" : accuracy_score,
         "f1_macro" : partial(f1_score, average="macro"),
     })
-
     print(moc_results_2)
+    '''
+
     if ifplot:
         plot_results(moc_results)
         plot_confusion_matrices(y_test, y_pred)
